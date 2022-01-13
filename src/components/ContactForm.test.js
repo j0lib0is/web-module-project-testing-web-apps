ca3a09facff8671d2b36f6597cdfb9d1727774b1
jacobjolibois <jacobjolibois@hey.com>
2022-01-13 14:49:09 -0600

    test 2

--- src/components/ContactForm.test.js
+++ src/components/ContactForm.test.js
@@ -10,11 +10,17 @@ test('renders without errors', ()=>{
 });
 
 test('renders the contact form header', ()=> {
-    
+	render(<ContactForm />);
+
+    const header = screen.queryByText(/contact form/i);
+
+	expect(header).toBeInTheDocument();
+	expect(header).toBeTruthy();
+    expect(header).toHaveTextContent(/contact form/i);
 });
 
 test('renders ONE error message if user enters less then 5 characters into firstname.', async () => {
-    
+	
 });
 
 test('renders THREE error messages if user enters no values into any fields.', async () => {
